516cf449182e5feb85bbd4665fb96ba2c7e96384
misty-spring <105533011+misty-spring@users.noreply.github.com>
2022-06-12 01:26:05 -0400

    Update Actions.cs

--- FarmVisitors/Actions.cs
+++ FarmVisitors/Actions.cs
@@ -1,9 +1,11 @@
-﻿using StardewModdingAPI;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Locations;
 using StardewValley.Network;
 using StardewValley.Objects;
 using System;
+using System.Collections.Generic;
 
 namespace FarmVisitors
 {
@@ -61,11 +63,14 @@ namespace FarmVisitors
         {
             if (Game1.currentLocation == farmHouse)
             {
-                Game1.drawDialogue(c, Values.GetRetireDialogue(c));
-                var position = farmHouse.getEntryLocation();
                 try
                 {
-                    c.controller = new PathFindController(c, farmHouse, position, 2);
+                    if(c.controller is not null)
+                    {
+                        c.Halt();
+                        c.controller = null;
+                    }
+                    Game1.fadeScreenToBlack();
                 }
                 catch (Exception ex)
                 {
@@ -73,6 +78,8 @@ namespace FarmVisitors
                 }
                 finally
                 {
+                    Game1.drawDialogue(c, Values.GetRetireDialogue(c));
+
                     ReturnToNormal(c, currentTime);
                     Game1.currentLocation.playSound("doorClose", NetAudio.SoundContext.NPC);
                 }
@@ -84,12 +91,74 @@ namespace FarmVisitors
         }
         internal static void ReturnToNormal(NPC c, int currentTime)
         {
-            c.ignoreScheduleToday = false;
-            c.followSchedule = true;
-            c.checkSchedule(currentTime);
+            try
+            {
+                c.Halt();
+                c.controller = null;
 
-            c.moveCharacterOnSchedulePath();
-            c.warpToPathControllerDestination();
+                c.ignoreScheduleToday = false;
+                c.followSchedule = true;
+
+                Game1.warpCharacter(c, "BusStop", new Point(2, 23));
+
+                c.InvalidateMasterSchedule();
+
+                c.checkSchedule(currentTime);
+
+                if (c.Schedule is null)
+                {
+                    var sched = c.getSchedule(Game1.dayOfMonth);
+                    c.Schedule.Clear();
+                    foreach (KeyValuePair<int, SchedulePathDescription> pair in sched)
+                    {
+                        c.Schedule.Add(pair.Key, pair.Value);
+                    }
+                }
+
+                c.moveCharacterOnSchedulePath();
+                c.warpToPathControllerDestination();
+            }
+            catch(Exception ex)
+            {
+                ModEntry.ModMonitor.Log($"Error while returning NPC: {ex}", LogLevel.Error);
+            }
+        }
+
+        private static string ParseSchedule(NPC c)
+        {
+            var time = Game1.timeOfDay;
+            var schedulename = c.dayScheduleName;
+            var schedule = c.getMasterScheduleEntry(schedulename);
+            string[] array = schedule.Split('/');
+            foreach(string s in array)
+            {
+                string[] schedulepart = s.Split(' ');
+                var scheduletime = Int16.Parse(schedulepart[0]);
+                if(scheduletime > time)
+                {
+                    string scheduleloc = schedulepart[1];
+                    return scheduleloc;
+                }
+            }
+            return "Town";
+        }
+
+        internal static string TurnToString(List<string> list)
+        {
+            string result = "";
+
+            foreach (string s in list)
+            {
+                if(s.Equals(list[0]))
+                {
+                    result = $"{s}";
+                }
+                else
+                {
+                    result = $"{result}, {s}";
+                }
+            }
+            return result;
         }
     }
 }
