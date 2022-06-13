d62a2404b0b3438b7a2f34a143093d12447631b0
misty-spring <105533011+misty-spring@users.noreply.github.com>
2022-06-13 02:38:30 -0400

    Update ModConfig.cs

--- FarmVisitors/ModConfig.cs
+++ FarmVisitors/ModConfig.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace FarmVisitors
 {
     internal class ModConfig
     {
-        public int CustomChance { get; set; } = 10;
+        public int CustomChance { get; set; } = 20;
         public int MaxVisitsPerDay { get; set; } = 3;
         public int StartingHours { get; set; } = 800;
         public int EndingHours { get; set; } = 2200;
-        public int Duration { get; set; } = 5;
+        public int Duration { get; set; } = 6;
         public bool Verbose { get; set; } = false;
+        public bool Debug { get; set; } = false;
     }
 }
