c5514c6e82cec958b69662b8355cc04bcc236970
misty-spring <105533011+misty-spring@users.noreply.github.com>
2022-06-13 02:37:36 -0400

    Update Values.cs

--- FarmVisitors/Values.cs
+++ FarmVisitors/Values.cs
@@ -1,32 +1,50 @@
-using Microsoft.Xna.Framework;
 using StardewValley;
-using System;
+using StardewValley.Locations;
+using StardewValley.Objects;
 using System.Collections.Generic;
 
 namespace FarmVisitors
 {
     internal class Values
     {
-        //internal static Dictionary<string, string> StringsFromCS = ModEntry.ModHelper.GameContent.Load<Dictionary<string, string>>("Strings\\StringsFromCSFiles");
         internal static bool IsMarriedToPlayer(NPC c)
         {
-            if(Game1.IsMultiplayer)
+            /*npc replacers will cause conflicts (friendshipdata checks internal but it seems to fail somehow)
+             * tried using c.displayName but to no avail
+            */
+            if (c is null)
+            {
+                return false;
+            }
+            else if (Game1.IsMultiplayer)
             {
                 foreach(Farmer eachFarmer in Game1.getAllFarmers())
                 {
+                    if(!eachFarmer.friendshipData.ContainsKey(c.Name))
+                    {
+                        ModEntry.ModMonitor.Log($"{c.Name} is not in the dictionary.");
+                        return true;
+                    }
                     //used to have "eachFarmer.spouse != null" inside the if
                     if (eachFarmer.IsMainPlayer && eachFarmer.friendshipData[c.Name].IsMarried())
                     {
+                        ModEntry.ModMonitor.Log($"{c.Name} is married!");
                         return true;
                     }
                 }
                 return false;
             }
             else
-            {
+            {         
                 Farmer player = Game1.MasterPlayer;
+                if (!player.friendshipData.ContainsKey(c.Name))
+                {
+                    ModEntry.ModMonitor.Log($"{c.Name} is not in the dictionary.");
+                    return true;
+                }
                 if (player.friendshipData[c.Name].IsMarried())
                 {
+                    ModEntry.ModMonitor.Log($"{c.Name} is married!");
                     return true;
                 }
                 else
@@ -95,52 +113,21 @@ namespace FarmVisitors
 
         internal static string GetTextOverHead(NPC instance)
         {
-            //Dictionary<string, string> StringsFromCS = ModEntry.ModHelper.GameContent.Load<Dictionary<string, string>>("Strings\\StringsFromCSFiles");
-
             var r = Game1.random.Next(1, 4);
 
             if (instance.SocialAnxiety.Equals(1)) //shy?
             {
-                //return StringsFromCS.GetValueOrDefault("NPC.cs.4071");
                 return ModEntry.ModHelper.Translation.Get($"NPCWalkIn.Shy{r}");
             }
             else
             {
-                //return StringsFromCS.GetValueOrDefault("NPC.cs.4068");
                 return ModEntry.ModHelper.Translation.Get($"NPCWalkIn.Outgoing{r}");
             }
-        }
-
-        internal static Point GetHouseSize()
-        {
-            Point point = new();
-
-            foreach (Farmer eachFarmer in Game1.getAllFarmers())
-            {
-                if (eachFarmer.IsMainPlayer)
-                {
-                    int level = eachFarmer.HouseUpgradeLevel;
-                    if(level is 0)
-                    {
-                        point = new Point(3, 10);
-                    }
-                    if(level is 1)
-                    {
-                        point = new Point(9, 10);
-                    }
-                    else
-                    {
-                        point = new Point(12, 19);
-                    }
-                }
-            }
-            return point;
         }
 
         internal static string GetSeasonalGifts()
         {
             string defaultgifts = "419 246 423 216 247 688 176 180 436 174 182 184 424 186 438"; //Vinegar, Wheat Flour, Rice, Bread, Oil, Warp Totem: Farm, small Egg (2 types),Large Egg (2 types), Goat Milk, Milk, Cheese, Large Milk, L. Goat Milk
-
             if (Game1.IsSpring)
             {
                 return $"{defaultgifts} 400 252 222 190 610 715"; //Strawberry, Rhubarb, Rhubarb Pie, Cauliflower, Fruit Salad, Lobster
@@ -161,6 +148,44 @@ namespace FarmVisitors
             {
                 return defaultgifts;
             }
+        }
+
+        internal static string TalkAboutFurniture(NPC c)
+        {
+            var r = Game1.random.Next(1, 4);
+
+            if (c.SocialAnxiety.Equals(1)) //shy?
+            {
+                return ModEntry.ModHelper.Translation.Get($"NPCFurniture.Shy{r}");
+            }
+            else
+            {
+                return ModEntry.ModHelper.Translation.Get($"NPCFurniture.Outgoing{r}");
+            }
+        }
+        internal static string GetRandomFurniture()
+        {
+            var list = ModEntry.FurnitureList;
+            int amount = list.Count;
+
+            //if there's no furniture, return null
+            if(amount is 0)
+            {
+                return null;
+            }
+
+            //choose random index and return itsdisplayname
+            var r = Game1.random.Next(0, (amount + 1));
+            return list[r].DisplayName;
+        }
+        internal static List<Furniture> UpdateFurniture(FarmHouse farmHouse)
+        {
+            List<Furniture> templist = new();
+            foreach (Furniture f in farmHouse.furniture)
+            {
+                templist.Add(f);
+            }
+            return templist;
         }
     }
 }
